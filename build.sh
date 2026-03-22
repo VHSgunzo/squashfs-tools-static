7a126839fad597c249eaeb0cd76afaba8cc01f0e
VHSgunzo <wosganzo@gmail.com>
2026-03-22 13:46:01 +0300

    Update build and v4.7.5

--- build.sh
+++ build.sh
@@ -3,8 +3,22 @@ set -e
 HERE="$(dirname "$(readlink -f "$0")")"
 cd "$HERE"
 
+SQUASHFS_TOOLS_VERSION=4.7.5
+
+MIMALLOC_VERSION=2.1.7
+
+XZ_VERSION=git              # 5.8.2
+LZO_VERSION=git             # no tags
+ZLIB_VERSION=git            # 1.3.2
+LZ4_VERSION=git             # 1.10.0
+ZSTD_VERSION=git            # 1.5.7
+SUPER_STRIP_VERSION=git     # 3.0a
+
 WITH_UPX=1
 VENDOR_UPX=1
+UPX_VERSION=4.2.4
+
+# NO_CLEANUP=1
 
 platform="$(uname -s)"
 platform_arch="$(uname -m)"
@@ -31,15 +45,14 @@ if [ "$WITH_UPX" == 1 ]
     then
         if [[ "$VENDOR_UPX" == 1 || ! -x "$(which upx 2>/dev/null)" ]]
             then
-                upx_ver=4.2.4
                 case "$platform_arch" in
                    x86_64) upx_arch=amd64 ;;
                    aarch64) upx_arch=arm64 ;;
                 esac
-                wget https://github.com/upx/upx/releases/download/v${upx_ver}/upx-${upx_ver}-${upx_arch}_linux.tar.xz
-                tar xvf upx-${upx_ver}-${upx_arch}_linux.tar.xz
-                mv upx-${upx_ver}-${upx_arch}_linux/upx /usr/bin/
-                rm -rf upx-${upx_ver}-${upx_arch}_linux*
+                wget https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-${upx_arch}_linux.tar.xz
+                tar xvf upx-${UPX_VERSION}-${upx_arch}_linux.tar.xz
+                mv upx-${UPX_VERSION}-${upx_arch}_linux/upx /usr/bin/
+                rm -rf upx-${UPX_VERSION}-${upx_arch}_linux*
         fi
 fi
 
@@ -69,7 +82,7 @@ echo "= build static deps"
 
 echo "= build mimalloc lib"
 (git clone https://github.com/microsoft/mimalloc.git && cd mimalloc
-git checkout v2.1.7
+[ "$MIMALLOC_VERSION" == 'git' ] || git checkout "v$MIMALLOC_VERSION"
 mkdir build && cd build
 (export CFLAGS="$CFLAGS -D__USE_ISOC11"
 cmake .. \
@@ -86,6 +99,7 @@ export CFLAGS="$CFLAGS -lmimalloc"
 
 (echo "= build lzma lib"
 (git clone https://git.tukaani.org/xz.git && cd xz
+[ "$XZ_VERSION" == 'git' ] || git checkout "v$XZ_VERSION"
 ./autogen.sh
 ./configure --enable-static --disable-shared
 make
@@ -93,36 +107,39 @@ cp -fv src/liblzma/.libs/liblzma.a /usr/lib/)
 
 echo "= build lzo2 lib"
 (git clone https://github.com/nemequ/lzo.git && cd lzo
+[ "$LZO_VERSION" == 'git' ] || git checkout "$LZO_VERSION"
 ./configure --enable-static --disable-shared
 make
 cp -fv src/.libs/liblzo2.a /usr/lib/)
 
 echo "= build zlib lib"
 (git clone https://github.com/madler/zlib.git  && cd zlib
+[ "$ZLIB_VERSION" == 'git' ] || git checkout "v$ZLIB_VERSION"
 ./configure
 make libz.a
 cp -fv libz.a /usr/lib/)
 
 echo "= build lz4 lib"
 (git clone https://github.com/lz4/lz4.git && cd lz4
+[ "$LZ4_VERSION" == 'git' ] || git checkout "v$LZ4_VERSION"
 make liblz4.a
 cp -fv lib/liblz4.a /usr/lib/)
 
 echo "= build zstd lib"
 (git clone https://github.com/facebook/zstd.git && cd zstd/lib
+[ "$ZSTD_VERSION" == 'git' ] || git checkout "v$ZSTD_VERSION"
 make libzstd.a
 cp -fv libzstd.a /usr/lib/))
 
 echo "= download squashfs-tools"
-git clone https://github.com/plougher/squashfs-tools.git
-squashfuse_version="$(cd squashfs-tools && git describe --long --tags|sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')"
-squashfs_tools_dir="${HERE}/build/squashfs-tools-${squashfs_tools_version}"
-mv "squashfs-tools" "${squashfs_tools_dir}"
-echo "= squashfs-tools v${squashfs_tools_version}"
+squashfs_tools_dir="${HERE}/build/squashfs-tools-${SQUASHFS_TOOLS_VERSION}"
+git clone https://github.com/plougher/squashfs-tools.git "$squashfs_tools_dir"
+echo "= squashfs-tools v${SQUASHFS_TOOLS_VERSION}"
 
 echo "= build squashfs-tools"
 (cd "${squashfs_tools_dir}"/squashfs-tools
-git checkout 4.7.2
+[ "$SQUASHFS_TOOLS_VERSION" == 'git' ] || git checkout "$SQUASHFS_TOOLS_VERSION"
+
 # patch -p2<"${HERE}/musl.patch"
 env XZ_SUPPORT=1 LZO_SUPPORT=1 LZ4_SUPPORT=1 ZSTD_SUPPORT=1 \
 make INSTALL_DIR="${squashfs_tools_dir}/install" LDFLAGS="$LDFLAGS" install)
@@ -135,6 +152,7 @@ done)
 
 echo "= build super-strip"
 (cd build && git clone https://github.com/aunali1/super-strip.git && cd super-strip
+[ "$SUPER_STRIP_VERSION" == 'git' ] || git checkout "$SUPER_STRIP_VERSION"
 make
 cp -fv sstrip /usr/bin/)
 
